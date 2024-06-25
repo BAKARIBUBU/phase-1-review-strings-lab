81e9a2d52e6d66160840bd0ff2e73f541915d953
bakari-rhoka-bubu <bakaribubu@gmail.com>
2024-06-25 07:16:08 +0300

    Strings

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+let currentUser= "bakari"
+let welcomeMessage=`Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage=`WELCOME TO FLATBOOK, ${currentUser.toLocaleUpperCase()}!`
+let shortGreeting= `Welcome, ${currentUser.slice(0,1)}!`
